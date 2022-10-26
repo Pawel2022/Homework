12d6672cefdbd6b0873636db321e320befe2c4c8
Pawel2022 <115695008+Pawel2022@users.noreply.github.com>
2022-10-26 20:39:37 +0200

    Add files via upload

--- bmi.py
+++ bmi.py
@@ -2,21 +2,20 @@
 def bmi():
     waga = float(input("waga:"))
     wzrost = float(input("wzrost:"))
-    waga/wzrost**2
-
-    if waga/wzrost**2 < 16:
+    y =  waga/wzrost**2
+    if y < 16:
         print("Severe Thinness")
-    if waga/wzrost**2 > 16 and waga/wzrost**2 <= 17:
+    if y > 16 and y <= 17:
         print("Moderate Thinness")
-    if waga/wzrost**2 > 17 and waga/wzrost**2 <= 18.5:
+    if y > 17 and y <= 18.5:
         print("Mild Thinness")
-    if waga/wzrost**2 > 18.5 and waga/wzrost**2 <= 25:
+    if y > 18.5 and y <= 25:
         print("Normal")
-    if waga/wzrost**2 > 25 and waga/wzrost**2 <= 30:
+    if y > 25 and y <= 30:
         print("Overweight")
-    if waga/wzrost**2 > 30 and waga/wzrost**2 <= 35:
+    if y > 30 and y <= 35:
         print("Obese Class I")
-    if waga/wzrost**2 > 35 and waga/wzrost**2 <= 40:
+    if y > 35 and y <= 40:
         print("Obese Class II")
     else:
         print("Obese Class III")
