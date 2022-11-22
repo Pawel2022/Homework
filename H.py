a70e9f9c0ab10eb9812acdbbce5f9e3bd0870b82
Pawel2022 <115695008+Pawel2022@users.noreply.github.com>
2022-11-22 19:33:09 +0100

    Add files via upload

---
+++ H.py
@@ -0,0 +1,15 @@
+qq = {
+"imie":"Anon",
+"nazwisko":"Anonski",
+"wiek":15,
+"praca":"uczeń liceum",
+"hobby":"4chan",
+"kuchnia":"niemiecka",
+"języki":"polski, angielski, niemiecki",
+"miejsce zamieszkania":"Warszawa",
+"zwierzak":"pies",
+"szkoła":"wyspiański"
+}
+for obj in qq:
+        for k, v in qq.items():
+            print(f"{k}: {v}")
