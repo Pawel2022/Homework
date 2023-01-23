6427ae5119a0b06c1af7ed50b74d196a7be31637
Pawel2022 <115695008+Pawel2022@users.noreply.github.com>
2023-01-23 20:11:24 +0100

    Add files via upload

---
+++ wzory.py
@@ -0,0 +1,152 @@
+class Wzory:
+    @staticmethod
+    def stezenie_procentowe():
+        ms = float(input())
+        mr = float(input())
+        print(ms/mr*100)
+
+    @staticmethod
+    def stezeniemolowe():
+        n = float(input())
+        v = float(input())
+        print(n/v)
+
+    @staticmethod
+    def stezenieprocentowe():
+        cp = float(input())
+        dr = float(input())
+        m = float(input)
+        print((cp*dr)/(m*100))
+
+    @staticmethod
+    def gestoscroztworu():
+        mr = float(input())
+        v = float(input())
+        print(mr/v)
+
+    @staticmethod
+    def masaroztworu():
+        ms = float(input())
+        mw = float(input())
+        print(ms+mw)
+
+    @staticmethod
+    def liczbamoli():
+        ms = float(input())
+        m = float(input())
+        print(ms/m)
+
+    @staticmethod
+    def stopiendysocjacjq():
+        cz = float(input())
+        c0 = float(input())
+        print(cz/c0*100)
+
+    @staticmethod
+    def stopiendysocjacjiw():
+        nz = float(input())
+        n0 = float(input())
+        print(nz/n0*100)
+
+    @staticmethod
+    def predkosc():
+        s = float(input())
+        t = float(input())
+        print(s/t)
+
+    @staticmethod
+    def przyspieszenie():
+        v = float(input())
+        t = float(input())
+        print(v/t)
+
+    @staticmethod
+    def predkoscwruchupookregu():
+        w = float(input())
+        r = float(input())
+        print(w*r)
+
+    @staticmethod
+    def przyspieszeniedosrodkowe():
+        v = float(input())
+        r = float(input())
+        print(v**2/r)
+
+    @staticmethod
+    def przyspieszeniekatowe():
+        w = float(input())
+        t = float(input())
+        print(w/t)
+
+    @staticmethod
+    def przyspieszeniestyczne():
+        e = float(input())
+        r = float(input())
+        print(e*r)
+
+    @staticmethod
+    def predkoscwprostoliniowymruchujednostejniezmiennym():
+        v = float(input())
+        a = float(input())
+        t = float(input())
+        print(v+a*t)
+
+    @staticmethod
+    def drogawprostoliniowymruchujednostajniezmiennym():
+        v = float(input())
+        t = float(input())
+        a = float(input())
+        print(v*t+0,5*a*t**2)
+
+    @staticmethod
+    def czestotliwoscq():
+        t = float(input())
+        print(1/t)
+
+    @staticmethod
+    def czestotliwoscw():
+        v = float(input())
+        t = float(input())
+        print(v*t)
+
+    @staticmethod
+    def ped():
+        m = float(input())
+        v = float(input())
+        print(m*v)
+
+    @staticmethod
+    def IIzasadadynamikiq():
+        p = float(input())
+        t = float(input())
+        print(p/t)
+
+    @staticmethod
+    def IIzasadadynamikiw():
+        f = float(input())
+        m =float(input())
+        print(f/m)
+
+    @staticmethod
+    def momentpedubtylysztywnej():
+        i = float(input())
+        w = float(input())
+        print(i*w)
+
+@staticmethod
+def moc():
+    w = float(input())
+    t = float(input())
+    print(w/t)
+
+@staticmethod
+def energiakinetyczna():
+    m = float(input())
+    v = float(input())
+    print(0,5*m*v**2)
+
+@staticmethod
+def energiakinetycznaruchuobrotowegobrylysztywnej():
+    i = float(input())
+    w = float(input())
+    print(0,5*i*w**2)
